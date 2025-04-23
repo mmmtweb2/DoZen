462ae211aa3e8207918b08eb583f489a99260808
Your Name <mmmtimoer@gmail.com>
2025-04-23 22:36:37 +0300

    share tasks $ folders

--- client/src/components/TaskItem.jsx
+++ client/src/components/TaskItem.jsx
@@ -70,7 +70,7 @@ function TaskItem({
         accessType: task.accessType
     });
     // Consistent permission checks
-    const isOwner = task.isOwner === true || task.user === localStorage.getItem('userId');
+    const isOwner = task.isOwner === true;
     const hasEditPermission = isOwner || task.accessType === 'edit';
     const canViewOnly = !isOwner && task.accessType === 'view';
 
@@ -416,11 +416,13 @@ function TaskItem({
                     <ul className="subtask-list">
                         {task.subtasks.map(subtask => (
                             <SubtaskItem
-                                key={subtask._id}
+                                key={subtask._id || `${task._id}-sub-${Math.random()}`}
                                 subtask={subtask}
                                 parentId={task._id}
+                                // Pass permissions down to SubtaskItem
                                 parentTaskAccessType={task.accessType}
                                 parentTaskIsOwner={isOwner}
+                                // Pass handlers
                                 onToggleSubtaskComplete={onToggleSubtaskComplete}
                                 onDeleteSubtask={onDeleteSubtask}
                                 onEditSubtask={onEditSubtask}
